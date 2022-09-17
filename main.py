9aeda29a79404b0bb2160a2373007120b950cbdd
Mert <62549656+fswair@users.noreply.github.com>
2022-09-17 15:23:50 +0300

    Logger V1
    First deploy!

---
+++ main.py
@@ -0,0 +1,40 @@
+from io import StringIO
+from typing import TextIO
+from enum import Enum
+import sys
+
+
+class LogLevels(Enum):
+    INFO = auto()
+    WARN = auto()
+    ERROR = auto()
+    DEFAULT = auto()
+
+
+class LoggerIO(object):
+    def __init__(self, name: str, buffer: TextIO = sys.stdout):
+        self.buffer = buffer
+
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls)
+
+    def log(
+        self, message: str, level: LogLevels = LogLevels.DEFAULT, io_mode: str = "a"
+    ) -> "LoggerIO":
+        prefix = ""
+        match (level):
+            case LogLevels.INFO:
+                prefix = "[INFO]"
+            case LogLevels.WARN:
+                prefix = "[WARN]"
+            case LogLevels.ERROR:
+                prefix = "[ERROR]"
+            case (_, LogLevels.DEFAULT):
+                prefix = "[LOG]"
+        self.buffer.mode = io_mode
+        io = StringIO(f"{prefix} {message}\n")
+        self.buffer.write(io.read())
+        return LoggerIO(self.buffer)
+
+    def __repr__(self):
+        return """logger.log('Instance created!', level=LogLevels.INFO)"""
